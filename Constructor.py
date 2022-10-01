b627164a8d7284a1e4bd7c0025e77ea27c4cd9eb
claviersouris <aamine.hadji.99@gmail.com>
2022-10-01 01:44:04 -0400

    Code Sample

--- Constructor.py
+++ Constructor.py
@@ -1,3 +1,17 @@
 #Name: Amine Hadji
 #Date: 01/10/2022
 
+####################
+# Consructors
+####################
+
+class Candidate:
+    def __init__(self, name, last_name, email):
+        self.name = name
+        self.last_name = last_name
+        self.email = email
+
+
+####################
+# Main
+####################
