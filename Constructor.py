69a5b37d4e544d853d3564faf42b373f70fb3fe1
claviersouris <aamine.hadji.99@gmail.com>
2022-11-07 15:57:58 -0500

    defs

--- Constructor.py
+++ Constructor.py
@@ -12,6 +12,9 @@
 # Consructors
 ####################
 
+import email
+
+
 class Candidate:
     def __init__(self, name, last_name, email):
         self.name = name
@@ -31,8 +34,8 @@ class Interviewer:
         self.created_job_Postings = created_job_Postings
 
 class Job_Posting:
-    def __init__(self, ID, title, description, num_of_jobOffers, interviewer, application_Forms):
-        self.ID = ID
+    def __init__(self, post_ID, title, description, num_of_jobOffers, interviewer, application_Forms):
+        self.post_ID = post_ID
         self.title = title
         self.description = description
         self.num_of_jobOffers = num_of_jobOffers
@@ -40,14 +43,14 @@ class Job_Posting:
         self.application_Forms = application_Forms
 
 class application_Forms:
-    def __init__(self, ID, candidate, jobPosting,CV):
-        self.ID = ID
+    def __init__(self, app_ID, candidate, jobPosting, CV):
+        self.app_ID = app_ID
         self.candidate = candidate
         self.jobPosting = jobPosting
         self.CV = CV
 
 class Candidate:
-    def __init__(self,candidate_ID,firstName,lastName,dateOfBirth,email,phoneNum,applicationForm,tentative_appointment,interview,is_archived):
+    def __init__(self,candidate_ID,firstName,lastName,dateOfBirth, email, phoneNum, applicationForm, tentative_appointment, interview, is_archived):
         self.candidate_ID = candidate_ID
         self.firstName = firstName
         self.lastName = lastName
@@ -60,27 +63,24 @@ class Candidate:
         self.is_archived = is_archived
 
 class Trainee(Candidate):
-    def __init_(self,trainee_ID,start_date):
+    def __init__(self,trainee_ID,start_date):
         self.trainee_ID = trainee_ID
         self.start_date = start_date
 
 class Interview:
-    def __init__(self,ID,candidate,interviewer,timeslot,interview_Results):
-        self.ID = ID
+    def __init__(self,Interview_ID,candidate,interviewer,timeslot,interview_Results):
+        self.Interview_ID = Interview_ID
         self.candidate = candidate
         self.interviewer = interviewer
-        self.timeslot = timeSlot
+        self.timeslot = timeslot
         self.interview_Results = interview_Results
 
 class TimeSlot:
-    def __init__(self, ID, interviewer,candidate,interview,date_time):
-        self.ID = ID
+    def __init__(self, interviewer,candidate,interview,date_time):
         self.interviewer = interviewer
         self.candidate = candidate
         self.interview = interview
         self.date_time = date_time
-
-
 
 
 ####################
@@ -88,17 +88,29 @@ class TimeSlot:
 ####################
 
 
-user2 = Interviewer("011",'Ariel','Piotraut','ariel@gmail','060606','10 sept, 11 sept','Week of 13th','Job1, Job2')
-print(user2.employee_ID, user2.firstName, user2.last_name, user2.email, user2.phoneNum, user2.planned_interview, user2.availabilities,user2.created_job_Postings)
-print(user2)
+user = Interviewer("011",'Ariel','Piotraut','ariel@gmail','060606','10 sept, 11 sept','Week of 13th','Job1, Job2')
+print(user.employee_ID, user.firstName, user.last_name, user.email, user.phoneNum, user.planned_interview, user.availabilities, user.created_job_Postings)
+print(user)
 
-
-
-jobPost = Job_Posting('01', 'BARISTA', 'being the barista', '5', user2.last_name, 'created') 
+jobPost = Job_Posting('01', 'BARISTA', 'being the barista', '5', user.last_name, 'created') 
 print(jobPost)
 print(jobPost.ID, jobPost.title, jobPost.description, jobPost.num_of_jobOffers, jobPost.interviewer, jobPost.application_Forms)
 
 
 
+############################
+# FUNCTIONS
+############################
 
+def is_available(Candidate, Interviewer):
+    interview = Interview(Interview_ID, Candidate, Interviewer, Candidate.tentative_appointment, 0)    
+    return interview 
 
+def send_timeSlots(self, Candidate,TimeSlot):
+    self.Candidate.tentative_appointments.append([TimeSlot])
+
+def inform(Interview, Candidate):
+    if Interview.interview_Result == True: 
+        Candidate.is_archived = False
+    else: 
+        Candidate.is_archived = True
